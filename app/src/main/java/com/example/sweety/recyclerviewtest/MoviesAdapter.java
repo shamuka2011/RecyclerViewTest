8b26437cbf781362001bd01bae223852bb07b3d6
shamuka2011 <shamuka2011@gmail.com>
2019-01-30 20:16:58 +0530

    Practice for recyclerview

---
+++ app/src/main/java/com/example/sweety/recyclerviewtest/MoviesAdapter.java
@@ -0,0 +1,62 @@
+package com.example.sweety.recyclerviewtest;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.Adapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Sweety on 30-01-2019.
+ */
+
+public class MoviesAdapter extends Adapter<MoviesAdapter.MyViewHolder> {
+
+    private List<Movie> moviesList;
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.movie_list_row, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        Movie movie = moviesList.get(position);
+        holder.title.setText(movie.getTitle());
+        holder.genre.setText(movie.getGenre());
+        holder.year.setText(movie.getYear());
+    }
+
+    @Override
+    public int getItemCount() {
+        return moviesList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView title, year, genre;
+
+        public MyViewHolder(View view) {
+            super(view);
+            title = (TextView) view.findViewById(R.id.title);
+            genre = (TextView) view.findViewById(R.id.genre);
+            year = (TextView) view.findViewById(R.id.year);
+        }
+    }
+
+    public MoviesAdapter(List<Movie> moviesList) {
+        this.moviesList = moviesList;
+    }
+
+
+
+
+
+}
